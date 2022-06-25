87f1474a85060b2972b4c17a1a4994a1f7f6f555
Tochi Chimezie <Torin>
2022-06-25 13:08:41 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const myName = "Oluwatorinmo Ajanaku";
+const height = "161cm";
+const country = "Nigeria";
+
+document.write(myName);
+document.write("<br>");
+document.write(height);
+document.write("<br>");
+document.write(country);
